b6a4b77e4729456dcd9929fe78f7bc396d109103
biswassuraj2024 <biswassuraj658@gmail.com>
2025-12-01 22:19:03 +0530

    prepared for production deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,23 +3,29 @@ import dotenv from "dotenv"
 import cors from 'cors'
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
-
+import path from 'path';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json())
-app.use(cors({
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
   origin:"http://localhost:5173"
-}))
+}))}
+
 /*app.use((req,res,next)=>{
   console.log("We just got a request")
   next();
 })*/
 
 app.use("/api/notes",notesRoutes)
+
+if (process.env.NODE_ENV === 'production') {app.use(express.static(path.join(__dirname, "../frontend/thinkboard/dist")));
+
+app.get("*", (req, res) => {res.sendFile(path.join(__dirname, "../frontend/thinkboard/dist/index.html"));})}
 connectDB().then(()=>{
 app.listen(PORT, () => {
   console.log('Server is running on port ',PORT);
